dd7fcbc3421d916fb0fceef2177d63bf6d6282ae
study-re-kailum <65376292+study-re-kailum@users.noreply.github.com>
2020-06-15 09:52:53 +0900

    blank update 2020-06-15

--- session_question03/confirm.php
+++ session_question03/confirm.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+	
 ?>
 
 <!DOCTYPE html>
@@ -9,12 +10,12 @@
 	</head>
 	<body>
 		■session_question03<br>
-		・form.phpで入力したcommentの値をdivタグ内で表示して下さい<br>
+		・form.phpで入力したcommentの値を使って、divタグ内で表示して下さい<br>
 		・確認できたらbackをクリックします<br>
 		<div style="border: 1px; background-color: lightsteelblue;">
 			<h2>Confirm</h2>
 			<div id="comment">
-				
+
 			</div>
 			<br>
 			<a href="form.php">back</a>
--- session_question04/welcome.php
+++ session_question04/welcome.php
@@ -12,11 +12,11 @@
 	</head>
 	<body>
 		■session_question04<br>
-		・login.phpで入力したusernameの値をh2タグ内で、Welcome 'username' !! のように表示して下さい<br>
-		・セッション変数へ格納したusernameがなければlogin.phpを表示するようにして下さい<br>
+		・login.phpで入力したusernameの値を使って、h2タグ内で「Welcome 'username' !!」のように表示して下さい<br>
+		・セッション変数に格納したusernameがなければlogin.phpを表示するようにして下さい<br>
 		※このページに直接URLを入力してアクセスした場合、ログイン無しで表示されてしまうことを阻止して下さい
 		<div style="border: 1px; background-color: lightsteelblue;">
-			<h2>Welcome <?php echo $_SESSION['username']; ?> !!</h2>
+			<h2>Welcome 'username' !!</h2>
 			<a href="logout.php">logout</a>
 		</div>
 
